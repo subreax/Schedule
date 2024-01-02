6587fd3c79c3e70135e2507c879d177fd0f09b90
subreax <dima2000947@gmail.com>
2024-01-02 14:59:02 +0300

    Transparent status bar

--- app/src/main/java/com/subreax/schedule/ui/welcome/EnterScheduleIdScreen.kt
+++ app/src/main/java/com/subreax/schedule/ui/welcome/EnterScheduleIdScreen.kt
@@ -2,10 +2,15 @@ package com.subreax.schedule.ui.welcome
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -45,57 +50,61 @@ fun EnterScheduleIdScreen(
     val hints = viewModel.hints
     val focusRequester = remember { FocusRequester() }
 
-    Column {
-        IconButton(onClick = goBack, modifier = Modifier.padding(start = 4.dp, top = 8.dp)) {
-            Icon(Icons.Filled.ArrowBack, "", tint = MaterialTheme.colorScheme.outline)
+    Box(modifier = Modifier.statusBarsPadding().imePadding().navigationBarsPadding()) {
+        Column(Modifier.fillMaxSize()) {
+            IconButton(onClick = goBack, modifier = Modifier.padding(start = 4.dp, top = 8.dp)) {
+                Icon(Icons.Filled.ArrowBack, "", tint = MaterialTheme.colorScheme.outline)
+            }
+            Text(
+                text = stringResource(R.string.initial_setup),
+                modifier = Modifier.padding(top = 10.dp, start = 16.dp, end = 16.dp),
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            Text(
+                stringResource(R.string.enter_your_schedule_id),
+                modifier = Modifier.padding(top = 8.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
+            )
+
+            OutlinedTextField(
+                value = viewModel.scheduleId,
+                onValueChange = viewModel::updateScheduleId,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .focusRequester(focusRequester),
+                label = { Text(stringResource(R.string.identifier)) },
+                isError = isError,
+                supportingText = {
+                    if (errorText != null) {
+                        Text(errorText.toString(LocalContext.current))
+                    }
+                },
+                singleLine = true
+            )
+
+            ScheduleIdHints(
+                hints = hints,
+                onClick = {
+                    viewModel.updateScheduleId(it)
+                    viewModel.submit()
+                },
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+            )
+
         }
-        Text(
-            text = stringResource(R.string.initial_setup),
-            modifier = Modifier.padding(top = 10.dp, start = 16.dp, end = 16.dp),
-            style = MaterialTheme.typography.titleLarge
-        )
-
-        Text(
-            stringResource(R.string.enter_your_schedule_id),
-            modifier = Modifier.padding(top = 8.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
-        )
-
-        OutlinedTextField(
-            value = viewModel.scheduleId,
-            onValueChange = viewModel::updateScheduleId,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp, vertical = 8.dp)
-                .focusRequester(focusRequester),
-            label = { Text(stringResource(R.string.identifier)) },
-            isError = isError,
-            supportingText = {
-                if (errorText != null) {
-                    Text(errorText.toString(LocalContext.current))
-                }
-            },
-            singleLine = true
-        )
-
-        ScheduleIdHints(
-            hints = hints,
-            onClick = {
-                viewModel.updateScheduleId(it)
-                viewModel.submit()
-            },
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxWidth()
-        )
 
         LoadingButton(
             text = stringResource(R.string.submit),
             isLoading = isLoading,
             onClick = viewModel::submit,
             modifier = Modifier
-                .align(Alignment.CenterHorizontally)
+                .align(Alignment.BottomCenter)
                 .padding(16.dp)
         )
+
     }
 
     LaunchedEffect(Unit) {

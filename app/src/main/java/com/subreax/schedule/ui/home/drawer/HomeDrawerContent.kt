f1878e11f6d40cb859dab3773a5c8710e4838c25
subreax <dima2000947@gmail.com>
2024-02-13 19:09:53 +0300

    Scrollable home drawer

--- app/src/main/java/com/subreax/schedule/ui/home/drawer/HomeDrawerContent.kt
+++ app/src/main/java/com/subreax/schedule/ui/home/drawer/HomeDrawerContent.kt
@@ -4,12 +4,15 @@ import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Tune
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -37,64 +40,66 @@ fun HomeDrawerContent(
     modifier: Modifier = Modifier
 ) {
     ModalDrawerSheet(windowInsets = WindowInsets(0.dp), modifier = modifier) {
-        Box(
-            modifier = Modifier
-                .aspectRatio(16f / 9f)
-                .fillMaxWidth()
-                .background(MaterialTheme.colorScheme.background)
-        ) {
-            Text(
-                text = "Расписание ТулГУ",
+        Column(Modifier.verticalScroll(rememberScrollState())) {
+            Box(
                 modifier = Modifier
-                    .padding(16.dp)
-                    .align(Alignment.BottomStart),
-                style = MaterialTheme.typography.titleMedium,
-                fontWeight = FontWeight.Bold
-            )
-        }
-
-        Text(
-            text = "Группы",
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp),
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.outline,
-            style = MaterialTheme.typography.bodyMedium
-        )
-
-        scheduleOwners.forEach {
-            SelectableDrawerItem(
-                selected = currentScheduleOwner.networkId == it.networkId,
-                onClick = { onScheduleOwnerClicked(it) },
+                    .aspectRatio(16f / 9f)
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.background)
             ) {
                 Text(
-                    text = it.toPrettyString(),
+                    text = "Расписание ТулГУ",
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp)
+                        .padding(16.dp)
+                        .align(Alignment.BottomStart),
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold
                 )
             }
-        }
 
-        DrawerItem(
-            onClick = navToScheduleOwnersManager,
-            modifier = Modifier
-                .padding(horizontal = 12.dp)
-                .fillMaxWidth()
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            Text(
+                text = "Группы",
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp),
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.outline,
+                style = MaterialTheme.typography.bodyMedium
+            )
+
+            scheduleOwners.forEach {
+                SelectableDrawerItem(
+                    selected = currentScheduleOwner.networkId == it.networkId,
+                    onClick = { onScheduleOwnerClicked(it) },
+                ) {
+                    Text(
+                        text = it.toPrettyString(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+                    )
+                }
+            }
+
+            DrawerItem(
+                onClick = navToScheduleOwnersManager,
+                modifier = Modifier
+                    .padding(horizontal = 12.dp)
+                    .fillMaxWidth()
             ) {
-                Icon(
-                    Icons.Outlined.Tune,
-                    contentDescription = "Редактор",
-                    tint = MaterialTheme.colorScheme.outline
-                )
-                Text(
-                    text = "Открыть редактор".uppercase(),
-                    fontSize = 12.sp,
-                    letterSpacing = 1.5.sp,
-                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Icon(
+                        Icons.Outlined.Tune,
+                        contentDescription = "Редактор",
+                        tint = MaterialTheme.colorScheme.outline
+                    )
+                    Text(
+                        text = "Открыть редактор".uppercase(),
+                        fontSize = 12.sp,
+                        letterSpacing = 1.5.sp,
+                    )
+                }
             }
         }
     }

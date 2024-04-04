a7cfff8a104901a5bb487fc1f58e005566f651e9
subreax <dima2000947@gmail.com>
2024-04-04 10:41:55 +0300

    Change icon background. Change version name and code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.subreax.schedule"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.0.2"
+        versionCode = 11
+        versionName = "1.1.0-dev1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

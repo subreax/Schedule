2f51a8cadb477f07c7818b8fcae6404dc37ca461
subreax <dima2000947@gmail.com>
2024-08-30 14:50:08 +0300

    Update commit hash in AboutScreen

--- app/src/main/java/com/subreax/schedule/ui/about/AboutScreen.kt
+++ app/src/main/java/com/subreax/schedule/ui/about/AboutScreen.kt
@@ -139,7 +139,7 @@ fun AboutScreen(navBack: () -> Unit) {
 
             Text(
                 /* TODO: Update on release */
-                text = stringResource(R.string.ver_s_date_commit_s, BuildConfig.VERSION_NAME, "8a6b4cd9"),
+                text = stringResource(R.string.ver_s_date_commit_s, BuildConfig.VERSION_NAME, "83abe340"),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(bottom = 4.dp),

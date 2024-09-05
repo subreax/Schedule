5a8b4526fde3b9095b07c973534d410cf686ad25
subreax <dima2000947@gmail.com>
2024-09-05 08:19:36 +0300

    Fix crashes in subject item title

--- app/src/main/java/com/subreax/schedule/ui/component/schedule/item/subject/TitleLayout.kt
+++ app/src/main/java/com/subreax/schedule/ui/component/schedule/item/subject/TitleLayout.kt
@@ -37,35 +37,50 @@ fun TitleLayout(
             error("Do not use this layout with 3 or more children")
         }
 
-        val maxNoteWidth = (constraints.maxWidth * MaxNoteWidthFraction).roundToInt()
-        val notePlaceable = measurables.getOrNull(1)?.measure(
-            constraints.copy(
-                minWidth = 0,
-                minHeight = 0,
-                maxWidth = maxNoteWidth
-            )
-        )
-        val noteWidth = notePlaceable?.width?.plus(gap.roundToPx()) ?: 0
-        val noteHeight = notePlaceable?.height ?: 0
+        val maxWidthPx = constraints.maxWidth
+        val gapPx = gap.roundToPx()
+
+        val maxTitleWidthPx = measurables.first().maxIntrinsicWidth(constraints.maxHeight)
+        val maxNoteWidthPx = measurables.getOrNull(1)?.maxIntrinsicWidth(constraints.maxHeight) ?: 0
+        val defaultMinNoteWidthPx = (maxWidthPx * MaxNoteWidthFraction).roundToInt()
+        val minNoteWidthPx = minOf(defaultMinNoteWidthPx, maxNoteWidthPx)
+
+
+        val titleWidthPx = if (maxNoteWidthPx == 0) {
+            maxTitleWidthPx.coerceAtMost(maxWidthPx)
+        } else {
+            maxTitleWidthPx.coerceAtMost(maxWidthPx - gapPx - minNoteWidthPx)
+        }
+
+        val noteWidthPx = (maxWidthPx - titleWidthPx - gapPx).coerceAtLeast(0)
 
         val titlePlaceable = measurables.first().measure(
             constraints.copy(
-                minWidth = 0,
+                minWidth = titleWidthPx,
+                maxWidth = titleWidthPx,
                 minHeight = 0,
-                maxWidth = constraints.maxWidth - noteWidth
+                maxHeight = constraints.maxHeight
             )
         )
-        val titleWidth = titlePlaceable.width
-        val titleHeight = titlePlaceable.height
 
-        val width = constraints.constrainWidth(titleWidth + noteWidth)
-        val height = constraints.constrainHeight(maxOf(titleHeight, noteHeight))
+        val notePlaceable = measurables.getOrNull(1)?.measure(
+            constraints.copy(
+                minWidth = noteWidthPx,
+                maxWidth = noteWidthPx,
+                minHeight = 0,
+                maxHeight = constraints.maxHeight
+            )
+        )
+
+        val width = constraints.constrainWidth(titleWidthPx + noteWidthPx)
+        val height =
+            constraints.constrainHeight(maxOf(titlePlaceable.height, notePlaceable?.height ?: 0))
         layout(width, height) {
             var x = 0f
             titlePlaceable.place(0, 0)
             x += titlePlaceable.width
 
-            if (notePlaceable != null) {
+            if (notePlaceable != null && notePlaceable.width > 0) {
                 x += gap.toPx()
                 val y = (height - notePlaceable.height) / 2f
                 notePlaceable.place(x.roundToInt(), y.roundToInt())
@@ -74,6 +89,17 @@ fun TitleLayout(
     }
 }
 
+
+@Composable
+private fun PreviewText(text: String) {
+    Text(
+        text = text,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis
+    )
+}
+
+
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 private fun TitleLayoutPreview() {
@@ -81,10 +107,10 @@ private fun TitleLayoutPreview() {
         Surface {
             TitleLayout(
                 title = {
-                    Text(text = "Заголовок")
+                    PreviewText("Заголовок")
                 },
                 note = {
-                    Text(text = "Примечание")
+                    PreviewText("(Примечание)")
                 },
                 modifier = Modifier
                     .padding(8.dp)
@@ -102,14 +128,10 @@ private fun LongTitleLayoutPreview() {
         Surface {
             TitleLayout(
                 title = {
-                    Text(
-                        text = "Длинный заголовок",
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
-                    )
+                    PreviewText("Длинный заголовок")
                 },
                 note = {
-                    Text(text = "Примечание")
+                    PreviewText("(Примечание)")
                 },
                 modifier = Modifier
                     .padding(8.dp)
@@ -126,14 +148,90 @@ private fun ExtraLongNoteTitleLayoutPreview() {
         Surface {
             TitleLayout(
                 title = {
-                    Text(
-                        text = "Длинный заголовок",
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
-                    )
+                    PreviewText(text = "Длинный заголовок заголовок")
                 },
                 note = {
-                    Text(text = "ТулГУ: А давай тут адрес хренакнем, чтобы приложуха крашилась")
+                    PreviewText(text = "(Очень длинный адрес)")
+                },
+                modifier = Modifier
+                    .padding(8.dp)
+                    .width(240.dp)
+            )
+        }
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun ExtraLongNoteTitleLayoutPreview2() {
+    ScheduleTheme {
+        Surface {
+            TitleLayout(
+                title = {
+                    PreviewText(text = "Заголовок")
+                },
+                note = {
+                    PreviewText(text = "(Очень длинный адрес)")
+                },
+                modifier = Modifier
+                    .padding(8.dp)
+                    .width(240.dp)
+            )
+        }
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun TitleLayoutPreview3() {
+    ScheduleTheme {
+        Surface {
+            TitleLayout(
+                title = {
+                    PreviewText(text = "Длинный заголовок фффф")
+                },
+                note = {
+                    PreviewText(text = "(1п/г)")
+                },
+                modifier = Modifier
+                    .padding(8.dp)
+                    .width(240.dp)
+            )
+        }
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun TitleLayoutPreview4() {
+    ScheduleTheme {
+        Surface {
+            TitleLayout(
+                title = {
+                    PreviewText(text = "Длинный заголовок очень длинный")
+                },
+                note = {
+                    PreviewText(text = "")
+                },
+                modifier = Modifier
+                    .padding(8.dp)
+                    .width(240.dp)
+            )
+        }
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun TitleLayoutPreview5() {
+    ScheduleTheme {
+        Surface {
+            TitleLayout(
+                title = {
+                    PreviewText(text = "")
+                },
+                note = {
+                    PreviewText(text = "Длинное примечание примечание выфафыввы")
                 },
                 modifier = Modifier
                     .padding(8.dp)

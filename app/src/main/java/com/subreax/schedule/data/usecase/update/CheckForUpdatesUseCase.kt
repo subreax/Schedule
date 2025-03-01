4058a33f358b49003d39183f17cdf32dfa520ce2
subreax <dima2000947@gmail.com>
2025-03-01 21:51:04 +0300

    Fix CheckForUpdatesUseCase

--- app/src/main/java/com/subreax/schedule/data/usecase/update/CheckForUpdatesUseCase.kt
+++ app/src/main/java/com/subreax/schedule/data/usecase/update/CheckForUpdatesUseCase.kt
@@ -2,13 +2,16 @@ package com.subreax.schedule.data.usecase.update
 
 import com.subreax.schedule.data.model.AppUpdateInfo
 import com.subreax.schedule.data.repository.update.UpdateRepository
+import com.subreax.schedule.utils.DateTimeUtils
 
 class CheckForUpdatesUseCase(
     private val updateRepository: UpdateRepository
 ) {
     suspend operator fun invoke(buildTime: Long): AppUpdateInfo? {
         return updateRepository.getLatestRelease().getValueOrNull()?.let {
-            if (buildTime < it.createdAt) {
+            val buildDate = DateTimeUtils.keepDateAndRemoveTime(buildTime)
+            val releaseDate = DateTimeUtils.keepDateAndRemoveTime(it.createdAt)
+            if (buildDate < releaseDate) {
                 it
             } else {
                 null
